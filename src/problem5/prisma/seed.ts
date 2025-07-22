e0c5ca555fa68f525debaa6c8b8a0244d448193d
Trí Nguyễn <tri.nguyen@devsamurai.com>
2025-07-22 20:50:28 +0700

    Feat: Problem 5: fix seeding

--- src/problem5/prisma/seed.ts
+++ src/problem5/prisma/seed.ts
@@ -12,10 +12,6 @@ async function main() {
   });
 }
 
-main()
-  .catch((e) => {
-    console.error(e);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+main().catch((e) => {
+  console.error(e);
+});
